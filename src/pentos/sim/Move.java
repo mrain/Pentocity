70d6a8e041740f78126fd764c9093bca5b0912ef
mrain <linmrain@gmail.com>
2016-09-29 13:32:45 -0400

    New simulator?

--- src/pentos/sim/Move.java
+++ src/pentos/sim/Move.java
@@ -13,21 +13,21 @@ public class Move {
     public Set<Cell> park;
 
     public Move(boolean accept) { // as a convention, if you reject a request you cannot build roads/water/parks. There is no need to anyway until you accept the next request
-		if (accept == false) {
-		    this.accept = false;
-		}
-		else {
-		    throw new RuntimeException("Must specify location and orientation of building.");
-		}
+	if (accept == false) {
+	    this.accept = false;
+	}
+	else {
+	    throw new RuntimeException("Must specify location and orientation of building.");
+	}
     }
 
     public Move(boolean accept, Building request, Cell location, int rotation, Set<Cell> road, Set<Cell> water, Set<Cell> park) {
-		this.accept = accept;
-		this.request = request;
-		this.location = location;
-		this.rotation = rotation;
-		this.road = road;
-		this.water = water;
-		this.park = park;
+	this.accept = accept;
+	this.request = request;
+	this.location = location;
+	this.rotation = rotation;
+	this.road = road;
+	this.water = water;
+	this.park = park;
     }
 }
